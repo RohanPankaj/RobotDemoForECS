299c2db3d66addc95c6b04978c3c81bf6f834570
Rohan Pankaj <rohansp01@gmail.com>
2019-06-06 10:07:56 -0700

    Should be ready for Demo

--- RobotDemoForECS/src/main/java/frc/robot/OI.java
+++ RobotDemoForECS/src/main/java/frc/robot/OI.java
@@ -12,31 +12,85 @@ package frc.robot;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+  //#region Controller Data
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+  public double getLeftYAxis() {
+    return condition_gamepad_axis(0.05, -gamepad1.getRawAxis(RobotMap.Buttons.leftYAxis), -1, 1);
+  }
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+  public double getLeftYAxis2() {
+    return condition_gamepad_axis(0.05, -gamepad3.getRawAxis(RobotMap.Buttons.leftYAxis), -1, 1);
+  }
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+  public double getRightXAxis() {
+    return condition_gamepad_axis(0.05, gamepad1.getRawAxis(RobotMap.Buttons.rightXAxis), -1, 1);
+  }
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+  public double getRightYAxis() {
+    return condition_gamepad_axis(0.05, -gamepad1.getRawAxis(RobotMap.Buttons.rightYAxis), -1, 1);
+  }
+
+  public int getPOV() { return gamepad3.getPOV(); }
+
+  /**
+   * Make the gamepad axis less sensitive to changes near their null/zero point.
+   * 
+   * @param value raw value from the gamepad axis
+   * @param dead  value for the deadband size 
+   * @return
+   */
+  public double deadBand(double value, double dead) {
+    if (Math.abs(value) < dead) {
+      return 0;
+    } else {
+      return value;
+    }
+  }
+
+  /**
+   * Clamp/Limit the value to only be within two limits
+   * @param min lower limit
+   * @param max upper limit
+   * @param val value to check
+   * @return
+   */
+
+  public double clamp(double min, double max, double val) {
+    if (min > val) {
+      return min;
+    } else if (max < val) {
+      return max;
+    } else {
+      return val;
+    }
+  }
+
+  /**
+   * Combine both a joystick limit and a clamp within standard limits.
+   * 
+   * I really wish programmers would name methods descriptively so that I do
+   * not have to waste my time figuring out what things like "bing" and "stuff" do!
+   * I am guessing that this does good "stuff" to my joystick. CCB.
+   * 
+   * @param dead deadband limit, no output within this limit. Normally 0.05
+   * @param val raw value from axis
+   * @param min lower limit for axis. Normally -1
+   * @param max upper limit on axis. Normally +1
+   * @return    conditioned value from axis (limited -1 to +1, with a )
+   */
+  public double condition_gamepad_axis(double dead, double val, double min, double max) {
+    return clamp(min, max, deadBand(val, dead));
+  }
+
+  //#endregion
+
+  //#region Configuration Methods
+
+  /**
+   * Purge the contorls
+   * 
+   * @deprecated Not need to remap controls. Just run one of the other configurations
+   */
+  
 }
